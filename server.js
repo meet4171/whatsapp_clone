14b974c4de6b9b69ffac4c6b36e8d1abd048572d
meet <meet45jadav@gmail.com>
2024-01-07 12:11:26 +0530

    shifted socket connection to server

--- server.js
+++ server.js
@@ -9,19 +9,21 @@ import { fileUpload, getFile } from './controller/UploadFile.js';
 import { connectDB } from './database/db.js';
 import { storage } from './utils/uploadFile.js';
 import path from 'path';
+import { Server } from "socket.io";
+import http from 'http'
+import { handleConnection } from './socket/index.js';
+
 dotenv.config();
-
-
 
 const upload = multer({ storage });
 
 const server = express();
+const httpServer = http.createServer(server);
 
 server.use('/', express.static(path.resolve('build')));
 
-
-server.use(cors());
 server.use(express.json());
+server.use(cors());
 server.use('/user', userRouter);
 server.use('/conversation', conversationRouter);
 server.use('/message', messageRouter);
@@ -29,13 +31,25 @@ server.use('/message', messageRouter);
 server.post('/file/upload', upload.single('file'), fileUpload);
 server.get('/file/get/:filename', getFile);
 
-
 server.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+  res.sendFile(path.join(path.resolve(import.meta.url, '../build'), 'index.html'));
+});
 
-server.listen(8080, () => {
-    console.log("Server Started at port http://localhost:8080");
+
+
+// Socket connection
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: ['http://localhost:3000', 'http://localhost:8080'],
+  }
+});
+
+handleConnection(io);
+
+
+httpServer.listen(8080, () => {
+  console.log("Server Started at port http://localhost:8080");
 });
 
 connectDB();
